6ce2f5a439343bd31aa245f2a329dcc3dc370c6b
alu0100502923 <alu0100502923@ull.edu.es>
2014-11-20 21:51:55 +0000

    subiendo mierdas

---
+++ lib/P5_T_34.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+# coding: utf-8
+require "P5_T_34/version"
+require "P5_T_34/Question"
+require "P5_T_34/LList"
+require "P5_T_34/Exam"
+require "P5_T_34/QuestionVF"
+
+module P5_T_34
+    
+end
---
+++ spec/P5_T_34_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'P5_T_34'
+
+
+module P5_T_34
+	#TO-DO
+end
+
