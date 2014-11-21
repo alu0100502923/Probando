e1cfa4fd8796b5d75404ccf4b1ad7fd6afb40a74
alu0100502923 <alu0100502923@ull.edu.es>
2014-11-21 00:47:19 +0000

    ESPERANZA

--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -99,19 +99,19 @@ class Exam
 
       it "Debe mostrarse correctamente" do
         text = "¿Cuál es la salida del siguiente código Ruby?\nclass Xyz\n\sdef pots\n\s\s@nice\n\send\nend\n\nxyz = Xyz.new\np xyz.pots"
-        examen = Lista.new(Question.new(:text => text, :right =>"nil", :distractors => ["#<Xyz:0xa000208>","0","Ninguna de las anteriores"]))
+        exam = Lista.new(Question.new(:text => text, :right =>"nil", :distractors => ["#<Xyz:0xa000208>","0","Ninguna de las anteriores"]))
 
         text = "La siguiente definición de un hash en Ruby es válida:\nhash_raro = {\n\s[1, 2, 3] => Object.new(),\nHash.new => :toto\n}"
-        examen << TrueFalse.new(:text => text, :right => false)
+        exam << TrueFalse.new(:text => text, :right => false)
 
         text = %Q{¿Cuál es la salida del siguiente código Ruby?\nclass Array\n\sdef say_hi\n\s\s"HEY!"\n\send\nend\n p [1, "bob"].say_hi}
-        examen << Question.new(:text => text, :right =>"HEY!", :distractors => ["1","bob","Ninguna de las anteriores"])
+        exam << Question.new(:text => text, :right =>"HEY!", :distractors => ["1","bob","Ninguna de las anteriores"])
    
         text = "¿Cuál es el tipo del objeto en el siguiente código Ruby?\nclass Objeto\nend"
-        examen << Question.new(:text => text, :right =>"Una instancia de la clase Class", :distractors => ["Una Constante", "Un Objeto", "Ninguna de las anteriores"])
+        exam << Question.new(:text => text, :right =>"Una instancia de la clase Class", :distractors => ["Una Constante", "Un Objeto", "Ninguna de las anteriores"])
    
         text = "Es apropiado que una clase Tablero herede de una clase Juego"
-        examen << TrueFalse.new(:text => text, :right => false)
+        exam << TrueFalse.new(:text => text, :right => false)
 	        
         expect(exam.to_s).to match(/(\d+\.-\)(.|\s|\n)+)+/)
       end
